3aeae20e47b071f18f2220b6776c5beaaddbfeb2
Joshua Stevenson <JoshuaStevenson97@gmail.com>
2016-09-24 06:02:43 +0100

    FSM example added!
    Some FSM examples, and a few classes to expand the FSM actions, such as
    "GetAngularVelocity2D" and "MultiplyFloatByTimeDelta"

---
+++ Assets/Scripts/PlayerExample/CustomPlayMakerActions/GetAngularVelocity2d.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Physics2D)]
+	[Tooltip("Gets the Angular Velocity of a Game Object and stores it in a Float Variable. NOTE: The Game Object must have a Rigid Body 2D.")]
+	public class GetAngularVelocity2d : FsmStateAction {
+
+		[RequiredField]
+		[CheckForComponent(typeof(Rigidbody2D))]
+		public FsmOwnerDefault gameObject;
+		[UIHint(UIHint.Variable)]
+		public FsmFloat velocity;
+		public bool everyFrame;
+
+		public override void Reset()
+		{
+			gameObject = null;
+			velocity = null;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			DoGetAngularVelocity();
+
+			if (!everyFrame)
+				Finish();		
+		}
+
+		public override void OnUpdate()
+		{
+			DoGetAngularVelocity();
+		}
+
+		void DoGetAngularVelocity()
+		{
+			GameObject go = Fsm.GetOwnerDefaultTarget(gameObject);
+			Rigidbody2D rigidbody = go.GetComponent<Rigidbody2D>();
+			if (go == null) return;
+			if (rigidbody == null) return;
+
+			float angularVelocity = rigidbody.angularVelocity;
+
+			velocity.Value = angularVelocity;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerExample/CustomPlayMakerActions/MutiplyFloatByDeltaTime.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+
+	[ActionCategory(ActionCategory.Time)]
+	[Tooltip("Mutiplies float value by Time.deltaTime or Time.fixedDeltaTime")]
+	public class MutiplyFloatByDeltaTime : FsmStateAction {
+
+		public enum TimeDelta{
+			DeltaTime,
+			FixedDeltaTime
+		}
+
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmFloat floatValue;
+		public TimeDelta timeDelta;
+		public bool everyFrame;
+
+		public override void Reset()
+		{
+			floatValue = null;
+			timeDelta = TimeDelta.DeltaTime;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			
+			DoMutiplication();
+
+			if (!everyFrame)
+				Finish();		
+		}
+
+		public override void OnUpdate()
+		{
+			DoMutiplication();
+		}
+
+		public override void OnFixedUpdate ()
+		{
+			DoMutiplication(true);
+		}
+
+		void DoMutiplication(bool fixedUpdateCall = false)
+		{
+			if(timeDelta == null) return;
+			if(floatValue == null) return;
+			if(timeDelta == TimeDelta.DeltaTime && !fixedUpdateCall){
+				floatValue.Value *= Time.deltaTime;
+			}else if(timeDelta == TimeDelta.FixedDeltaTime && fixedUpdateCall){
+				floatValue.Value *= Time.fixedDeltaTime;
+			}
+		}
+	}
+}
